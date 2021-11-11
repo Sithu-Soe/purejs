71811acc12f2ff2c7c73f614b97c1830b447fe42
TkNSithu <soesithu908@gmail.com>
2021-11-11 15:09:05 +0630

    fetch test

--- fetch.js
+++ fetch.js
@@ -1,4 +1,5 @@
-fetch('http://xtmm.live:8080/player_api.php?username=happylive&password=happylive&action=get_live_categories')
+const url = 'http://xtmm.live:8080/';
+fetch(url + 'player_api.php?username=happylive&password=happylive&action=get_live_categories')
   .then((response) => {
       console.log(response, 'dddd')
     return response.json();
