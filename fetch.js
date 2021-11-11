7d16f5c7a7632e8730473e1e3bc7b24e34bda34a
TkNSithu <soesithu908@gmail.com>
2021-11-11 15:13:00 +0630

    remove credential

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
 const url = 'http://xtmm.live:8080/';
-fetch(url + 'player_api.php?username=happylive&password=happylive&action=get_live_categories')
+fetch(url + 'player_api.php?username=plzaddusername&password=plzaddpassword&action=get_live_categories')
   .then((response) => {
       console.log(response, 'dddd')
     return response.json();
